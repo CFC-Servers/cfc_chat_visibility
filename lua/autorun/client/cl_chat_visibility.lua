595fd48f6a6cead5a1a7bd52e09eee236be754cb
PLlally <11771225+PLally@users.noreply.github.com>
2020-01-23 00:34:37 -0500

    more whitespace changes

--- lua/autorun/client/cl_chat_visibility.lua
+++ lua/autorun/client/cl_chat_visibility.lua
@@ -1,4 +1,4 @@
-net.receive("CFC_ChatVisibility_print", function(n)
+net.receive( "CFC_ChatVisibility_print", function(n)
     msgInfo = net.ReadTable()
-    chat.AddText(msgInfo.message)
+    chat.AddText( msgInfo.message )
 end)
