e1d418fc485f87ff2ee88e76affd015446e79b11
PLlally <11771225+PLally@users.noreply.github.com>
2020-01-23 00:18:11 -0500

    inital commit

---
+++ lua/autorun/client/cl_chat_visibility.lua
@@ -0,0 +1,4 @@
+net.receive("CFC_ChatVisibility_print", function(n)
+    msgInfo = net.ReadTable()
+    chat.AddText(msgInfo.message) 
+end)
---
+++ lua/autorun/server/sv_chat_visibility.lua
@@ -0,0 +1,74 @@
+util.AddNetworkString( "CFC_ChatVisibility_print" )
+local colors = {
+    team_label = Color(0,255,0),
+    psay_seperator = Color(0, 180, 90),
+    text = Color(255, 255, 255)
+}
+
+local format = string.format
+
+local function isStaff(ply)
+    return ply:IsAdmin()
+end
+
+local function teamColor(ply)
+    return team.GetColor(ply:GetTeam())
+end
+
+local function printMessage(ply, msg) 
+    net.Start("CFC_ChatVisibility_print")
+    net.WritTable(msg)
+    net.Send(ply)
+end
+
+local function printStaff(msg) 
+    for _, ply in pairs(player.GetHumans()) do
+        if isStaff(ply) and msg:shouldPrint(ply) then
+            printMessage(ply, msg)
+        end
+    end
+
+end
+
+
+hook.Add("PlayerSay", "CFC_ChatVisibility_Say", function( ply, text, isTeam ) 
+    if not isTeam then return end
+
+    local message = {
+        author = ply,
+        message = {
+            colors.team_label, "(team)", 
+            teamColor(ply), ply:GetName(), 
+            colors.text ":", text 
+        },
+        msgType = "TEAM",
+        shouldPrint = function(msg, ply)
+            return msg.author:GetTeam() ~= ply:GetTeam()
+        end
+    }
+    
+    printStaff(message)
+end) 
+
+hook.Add("ULibCommandCalled", "CFC_ChatVisibility_ULibCommand", function(ply, cmd, args)
+    if cmd ~= "ulx psay" then return end
+    author = args[1]
+    recipient = args[2]
+    text = args[3]
+
+    printStaff{
+        author = author,
+        recipient = recipient,
+        message = {
+            teamColor(author), author:GetName(),
+            colors.psay_seperator, "->",
+            teamColor(recipient), recipient:GetName(),
+            colors.text, ":", text
+        },
+        msgType = "PSAY",
+        shouldPrint = function(msg, ply)
+            return msg.author ~= ply and msg.recipient ~= ply 
+        end
+    }
+
+end)
