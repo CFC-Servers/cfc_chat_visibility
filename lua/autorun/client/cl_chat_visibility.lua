71e80dc8ac1b088a128d62eaaaf3bf711ab0c56b
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-02-15 14:27:55 -0500

    Fix spacing

--- lua/autorun/client/cl_chat_visibility.lua
+++ lua/autorun/client/cl_chat_visibility.lua
@@ -1,4 +1,4 @@
-net.Receive( "CFC_ChatVisibility_print", function(n)
+net.Receive( "CFC_ChatVisibility_print", function( n )
     message = net.ReadTable()
     chat.AddText( unpack( message ) )
-end)
+end )
