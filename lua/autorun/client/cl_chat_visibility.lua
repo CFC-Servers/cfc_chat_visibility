c4c575c80480aacad1c29d0bd141aeda92b195bf
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-11-29 00:04:09 +0000

    Add Convar

--- lua/autorun/client/cl_chat_visibility.lua
+++ lua/autorun/client/cl_chat_visibility.lua
@@ -1,4 +1,8 @@
+local enabledConvar = CreateClientConVar( "cfc_chatvisibility_enabled", "1", true, false)
+
 net.Receive( "CFC_ChatVisibility_print", function( n )
+    if not enabledConvar:GetBool() then return end
+
     message = net.ReadTable()
     chat.AddText( unpack( message ) )
 end )
