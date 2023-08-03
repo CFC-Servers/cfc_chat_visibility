399ee8c74552036b288551d826e0fba05a04824e
Redox <mail@redox.to>
2023-08-03 04:18:20 +0100

    Style

--- lua/autorun/client/cl_chat_visibility.lua
+++ lua/autorun/client/cl_chat_visibility.lua
@@ -1,6 +1,6 @@
 local enabledConvar = CreateClientConVar( "cfc_chatvisibility_enabled", "1", true, false )
 
-net.Receive( "CFC_ChatVisibility_print", function( n )
+net.Receive( "CFC_ChatVisibility_print", function()
     if not enabledConvar:GetBool() then return end
 
     message = net.ReadTable()
